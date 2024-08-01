8797089932bc6afbba4f5cc21fa87521da85a720
RS_ict <r.stefani87.informatica@gmail.com>
2024-08-02 01:33:21 +0200

    feat: images and fonts mapping production

---
+++ styles/fonts.js
@@ -0,0 +1,20 @@
+
+import path from 'path';
+import * as files from '@ares/files';
+
+ function fontFamilies(fontDirectory, mappingFile) {
+  const fontFiles = fs.readdirSync(fontDirectory);
+  const ret = {};
+  fontFiles.forEach(fontFile => {
+    const fontPath = path.join(fontDirectory, fontFile, `${fontFile}.ttf`);
+    const fontData = fs.readFileSync(fontPath);
+    const fontBase64 = fontData.toString('base64');
+    ret[fontFile] = {
+      fontFamily: fontFile,
+      src: `url(data:font/ttf;charset=utf-8;base64,${fontBase64})`,
+    };
+  });
+  files.setFileContent(mappingFile, JSON.stringify(ret), 'utf8');  
+}
+
+export default fontFamilies();
---
+++ styles/images.js
@@ -0,0 +1,16 @@
+
+import path from 'path';
+import * as files from '@ares/files';
+
+function images( imageDirectory, mappingFile) {
+    const imageFiles = files.getFilesRecursively(imageDirectory, /^*.(bmp|png|jpg|jpeg|svg|gif|webp|url)$/i , true);
+    const ret = {};
+    imageFiles.forEach(imageFile => {
+        const imagePath = path.join(imageDirectory, imageFile);
+        ret[files.getFileNameAsPropertyName(imageFile)] = Image.resolveAssetSource(imagePath);
+    });
+    files.setFileContent(mappingFile, JSON.stringify(ret), 'utf8'); 
+}
+
+export default images();
+
