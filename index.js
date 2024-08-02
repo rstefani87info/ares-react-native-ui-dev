633f966863c0b93c210d40626c6d3f9890409e11
RS_ict <r.stefani87.informatica@gmail.com>
2024-08-03 00:14:32 +0200

    fix: index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ if(command.charAt(':')>-1){
     command = command[0];
 }
 
-const params = args.slice(1).map(p=> Number(p) || p);
-console.log("Executing command: ", command, subfunction, params);
+console.log("Executing command: ", command, subfunction);
 try {
     command = (await import('./' + command+ '.js')).default; 
     if(subfunction){
@@ -18,8 +17,9 @@ try {
     else {
         subfunction = command.default;
     }
-    console.debug("Params:",params);
-    // subfunction(...params);
+    const paramSet= args.slice(1).map(p=> Number(p) || p);
+    console.debug("Params:",paramSet);
+    subfunction(...paramSet);
 } catch (err) {
     console.error(err);
 }
