74120589ef8bb13611711f123dc680699117a62d
RS_ict <r.stefani87.informatica@gmail.com>
2025-05-02 15:04:58 +0200

    fix: setup.js sets script commands on  package.json of host node root only

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,23 +1,24 @@
-import { getFileContent, setFileContent, getFile } from '@ares/files';
+import { getFileContent, setFileContent, getFile } from "@ares/files";
 
 try {
-  const projectRoot = process.cwd();
-  const packageJsonPath = getFile(projectRoot, 'package.json');
+  const projectRoot = process.env.INIT_CWD;
+  if (projectRoot) {
+    const packageJsonPath = getFile(projectRoot, "package.json");
 
-  const packageJsonContent = getFileContent(packageJsonPath);
-  if (!packageJsonContent) throw new Error('package.json content could not be retrieved.');
+    const packageJsonContent = getFileContent(packageJsonPath);
+    if (!packageJsonContent)
+      throw new Error("package.json content could not be retrieved.");
 
-  const packageJson = JSON.parse(packageJsonContent);
+    const packageJson = JSON.parse(packageJsonContent);
 
-  packageJson.scripts = {
-    ...packageJson.scripts,
-    start: 'node node_modules/@ares/react-native-ui-dev/start.js'
-  };
+    packageJson.scripts = {
+      ...packageJson.scripts,
+      start: "node node_modules/@ares/react-native-ui-dev/start.js",
+    };
 
-  setFileContent(packageJsonPath, JSON.stringify(packageJson, null, 2));
-  console.log('Script "start" successfully updated in package.json');
-
+    setFileContent(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    console.log('Script "start" successfully updated in package.json');
+  }
 } catch (error) {
-  console.error('Error updating package.json:', error.message);
+  console.error("Error updating package.json:", error.message);
 }
-
